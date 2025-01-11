34936a80b22b6bc4ce5a6b59b26b6e115c211677
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-11 20:56:57 +0800

    feat(db): create student model

---
+++ backend/src/models/student.model.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/index.js";
+
+export const Student = sequelize.define('Student', {
+  student_id: { // Student USN
+    type: DataTypes.INTEGER,
+    autoIncrement: true, // autoincrement for development purposes only
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  strandProgram: { // Animation or BSIT
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  section: { // Section Gumamela
+    type: DataTypes.STRING,
+    allowNull: false,
+  }, 
+  yearLevel: { // 2nd year, or 11th grader
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  schoolLevel: { // SHS or College
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+  
