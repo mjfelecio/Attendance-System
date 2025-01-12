55465a900ccfcf6c68cfdac7988c8ff1d3de67b9
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-12 21:42:47 +0800

    feat(frontend): Implement Navbar and setup frontend
    - Add Chakra UI as a component library for easier development
    - Set up frontend dependencies and configs

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import { Container, Text } from '@chakra-ui/react'
+
+const Dashboard = () => {
+  return (
+    <Container maxW={"container.xl"} py={12} background={"gray.400"}>
+      <Text color={"white"}>DASHBOARD Placeholder</Text>
+    </Container>
+  )
+}
+
+export default Dashboard
