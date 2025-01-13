6ff7c80dc1cabc630fa4948c28aa96aff5b72118
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-13 15:18:08 +0800

    feat(ui): Add Sidebar template

---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react"
+
+const Sidebar = () => {
+  return (
+    <Box bgColor={"gray.700"} w={"300px"} minH={"100vh"} >
+        Sidebar
+    </Box>
+  )
+}
+
+export default Sidebar
