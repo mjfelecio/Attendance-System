e0924e1dc9d0fc0aa066f8bc9e8a38f4b275cf89
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-18 14:06:09 +0800

    feat(api): Implement basic CRUD operations on event data
    - Add fetchEvents to fetch all events
    - Add editEvent to edit event details
    - Add deleteEvent to delete an event
    - Add fetchEvent to fetch a specific event using its eventId

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -20,3 +20,117 @@ export const createEvent = async (req, res) => {
     }
 };
 
+export const fetchEvents = async (req, res) => {
+    try {
+        const events = await Event.findAll();
+
+        if (!events || events.length === 0) throw new Error("No event has been created");
+
+        res.status(201).json({
+            success: true,
+            message: "Events fetched successfully",
+            data: events,
+        });
+    } catch (error) {
+        if (error.message.includes("No event")) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while fetching the events",
+        });
+    }
+};
+
+export const editEvent = async (req, res) => {
+    const { eventId } = req.params;
+    const editDetails = req.body;
+
+    try {
+        const event = await Event.findByPk(eventId);
+
+        if (!event) throw new Error("Event not found");
+
+        const editedStudent = await event.update(editDetails);
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully edited event details",
+            data: editedStudent,
+        });
+    } catch (error) {
+        if (error.message.includes("not found")) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while editing event details",
+        });
+    }
+};
+
+export const deleteEvent = async (req, res) => {
+    const { eventId } = req.params;
+
+    try {
+        const event = await Event.findByPk(eventId);
+
+        if (!event) throw new Error("Event not found");
+
+        await event.destroy({ force: true });
+
+        res.status(200).json({
+            success: true,
+            message: "Successfully deleted event",
+        });
+    } catch (error) {
+        if (error.message.includes("not found")) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while deleting event",
+        });
+    }
+};
+
+export const fetchEvent = async (req, res) => {
+    const { eventId } = req.query;
+
+    try {
+        const event = await Event.findByPk(eventId);
+
+        if (!event) throw new Error("Event not found");
+
+        res.status(200).json({
+            success: true,
+            message: "Event found",
+            data: event,
+        });
+    } catch (error) {
+        if (error.message.includes("not found")) {
+            return res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+        
+        res.status(500).json({
+            success: false,
+            message: "An error occurred while fetching event",
+        });
+    }
+};
+
