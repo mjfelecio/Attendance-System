c77b9a09a31f455f90df3a7cbd994da78bade79b
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-18 14:20:36 +0800

    feat(api): Add event routes for CRUD operations

--- backend/src/routes/event.route.js
+++ backend/src/routes/event.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { createEvent } from "../controllers/event.controller.js";
+import { createEvent, deleteEvent, editEvent, fetchEvent, fetchEvents } from "../controllers/event.controller.js";
 
 const router = express.Router();
 
 router.post("/", createEvent);
+router.get("/", fetchEvents);
+router.put("/:eventId", editEvent);
+router.delete("/:eventId", deleteEvent);
+router.get("/fetch?", fetchEvent);
 
 export default router;
