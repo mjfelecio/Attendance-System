8c4430eceed566318bcc0afd3f70e59e77cf3ca9
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2025-01-21 13:18:13 +0800

    feat(model): Define model associations for the tables
    - Add an index.js file to define any association  between the models. This prevents cases where associations are defined before the models are even created.
    - Export all models from here instead. This provides a centralized location to handle cross model operations and makes it easier to add or remove models in the future.

---
+++ backend/src/models/index.js
@@ -0,0 +1,11 @@
+import { Event } from "./event.model.js";
+import { Student } from "./student.model.js";
+import { Attendance } from "./attendance.model.js";
+
+// Model associations
+Event.hasMany(Attendance, { foreignKey: "eventId" });
+Student.hasMany(Attendance, { foreignKey: "studentId" });
+Attendance.belongsTo(Event, { foreignKey: "eventId" });
+Attendance.belongsTo(Student, { foreignKey: "studentId" });
+
+export { Event, Student, Attendance };
