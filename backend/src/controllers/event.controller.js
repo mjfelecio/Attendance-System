f63302381959c9c062727c5526b4c307ba5e432a
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-03-29 16:59:13 +0800

    feat: Change fetching empty event status code to 204
    The old status code was 404, which signalled that something critical. Change it to 204 to more accurately reflect it's purpose.
    
    Because by default, a user has to create an event first, it doesn't make sense to show an error before doing anything yet.

--- backend/src/controllers/event.controller.js
+++ backend/src/controllers/event.controller.js
@@ -33,7 +33,7 @@ export const fetchEvents = async (req, res) => {
         });
     } catch (error) {
         if (error.message.includes("No event")) {
-            return res.status(404).json({
+            return res.status(204).json({
                 success: false,
                 message: error.message,
             });
