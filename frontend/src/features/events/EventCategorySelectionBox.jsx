01961f9cf577cf378ed44f041ed1b3dbad58d983
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-06-17 13:41:28 +0800

    feat: Create EventCategorySelectionBox component

---
+++ frontend/src/features/events/EventCategorySelectionBox.jsx
@@ -0,0 +1,55 @@
+"use client"
+
+import {
+  Combobox,
+  useFilter,
+  useListCollection,
+} from "@chakra-ui/react"
+
+export const EventCategorySelectionBox = () => {
+  const { contains } = useFilter({ sensitivity: "base" })
+
+  const { collection, filter } = useListCollection({
+    initialItems: eventTypes,
+    filter: contains,
+  })
+
+  return (
+    <Combobox.Root
+      collection={collection}
+      onInputValueChange={(e) => filter(e.inputValue)}
+      width="240px"
+	  openOnClick
+    >
+      <Combobox.Label>Select Event Type</Combobox.Label>
+      <Combobox.Control>
+        <Combobox.Input placeholder="Type to search" />
+        <Combobox.IndicatorGroup>
+          <Combobox.ClearTrigger />
+          <Combobox.Trigger />
+        </Combobox.IndicatorGroup>
+      </Combobox.Control>
+        <Combobox.Positioner>
+          <Combobox.Content>
+            <Combobox.Empty>No items found</Combobox.Empty>
+            {collection.items.map((item) => (
+              <Combobox.Item item={item} key={item.value}>
+                {item.label}
+                <Combobox.ItemIndicator />
+              </Combobox.Item>
+            ))}
+          </Combobox.Content>
+        </Combobox.Positioner>
+    </Combobox.Root>
+  )
+}
+
+const eventTypes = [
+  { label: "School Event", value: "school" },
+  { label: "College Event", value: "college" },
+  { label: "SHS Event", value: "shs" },
+  { label: "Department Event", value: "dept" },
+  { label: "House Event", value: "house" },
+  { label: "Year Specific Event", value: "year" },
+  { label: "Custom", value: "custom" },
+]
